be160b2dd6b385ac224f42bc2014b4ba68c3d45d
IsaacCodes <179248168+IsaacCodes@users.noreply.github.com>
2026-08-04 19:39:59 -0700

    day01 cleanup

--- day01/src/v1.rs
+++ day01/src/v1.rs
@@ -3,14 +3,17 @@ use std::{cmp::max, fs::File, io::{BufRead, BufReader}};
 pub fn v1() {
     //Read file into line reading buffer
     let file = File::open("src/input.txt").unwrap();
-    let lines = BufReader::new(file).lines();
-
+    let lines = BufReader::new(file)
+        .lines()
+        .map_while(Result::ok)
+        //Adds sentinel empty line
+        .chain(std::iter::once(String::new()));
 
     //State vars
     let mut highest = 0;
     let mut current = 0;
     //Read each line
-    for line in lines.map_while(Result::ok) {
+    for line in lines {
         //Reset on empty
         if line.is_empty() {
             highest = max(highest, current);
@@ -21,8 +24,6 @@ pub fn v1() {
             current += line.parse::<i32>().unwrap();
         }
     }
-    //Catch last current
-    highest = max(highest, current);
 
     println!("{highest}")
 }
--- day01/src/v2.rs
+++ day01/src/v2.rs
@@ -3,13 +3,17 @@ use std::{cmp::Reverse, collections::BinaryHeap, fs::File, io::{BufRead, BufRead
 pub fn v2() {
     //Read file into line reading buffer
     let file = File::open("src/input.txt").unwrap();
-    let lines = BufReader::new(file).lines();
+    let lines = BufReader::new(file)
+        .lines()
+        .map_while(Result::ok)
+        //Adds sentinel empty line
+        .chain(std::iter::once(String::new()));
 
     //State vars
     let mut min_heap = BinaryHeap::new();
     let mut current = 0;
     //Read each line
-    for line in lines.map_while(Result::ok).peekable() {
+    for line in lines {
         //Reset on empty
         if line.is_empty() {
             //Push item to heap
@@ -24,12 +28,6 @@ pub fn v2() {
         else {
             current += line.parse::<i32>().unwrap();
         }
-    }
-
-    //Catch last item
-    min_heap.push(Reverse(current));
-    if min_heap.len() > 3 {
-        min_heap.pop();
     }
 
     //Sum 3 items on min heap
