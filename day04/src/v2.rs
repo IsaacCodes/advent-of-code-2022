1ab546afaeb25b24076ec272b6d44631dc341f4e
IsaacCodes <179248168+IsaacCodes@users.noreply.github.com>
2026-08-07 13:43:47 -0700

    day04

---
+++ day04/src/v2.rs
@@ -0,0 +1,32 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+use itertools::Itertools;
+
+pub fn v2() {
+    //Read file into line reading buffer
+    let file = File::open("src/input.txt").unwrap();
+    let lines = BufReader::new(file)
+        .lines()
+        .map_while(Result::ok);
+
+
+    //State vars
+    let mut total = 0;
+    //Read each line
+    for line in lines {
+        //This monstrosityy parse s1-e1,s2-e2
+        let ((s1, e1), (s2, e2)) = line
+            .split(",")
+            .map(
+                |range| range
+                .split("-")
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect_tuple().unwrap()
+            )
+            .collect_tuple().unwrap();
+
+        //Range 1 overlaps with 2
+        total += (e2 >= s1 && s2 <= e1) as u32;
+    }
+
+    println!("{total}");
+}
