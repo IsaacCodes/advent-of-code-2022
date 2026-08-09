93d6008c892aded565dacf432b32d9455d72f5f0
IsaacCodes <179248168+IsaacCodes@users.noreply.github.com>
2026-08-08 21:08:48 -0700

    day05

---
+++ day05/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+
+mod v1;
+mod v2;
+
+use v1::v1;
+use v2::v2;
+
+fn main() {
+    //CLI args iterator
+    let mut args = env::args();
+    //Get away program name
+    let program_name = args.next().unwrap();
+    //Match 1 or 2 to version
+    let version = args.next();
+    match version.as_deref() {
+        //Call appropriate version
+        Some("1") => v1(),
+        Some("2") => v2(),
+        _ => println!("Usage: {program_name} [1 or 2]"),
+    }
+}
---
+++ day05/src/v1.rs
@@ -0,0 +1,80 @@
+use std::{fs::File, io::{BufRead, BufReader}};
+
+struct Crates {
+    core: Vec<Vec<char>>,
+}
+
+impl Crates {
+    fn from_diagram(diagram: Vec<Vec<char>>) -> Self {
+        //Gets character at [-1][-2] and converts it to an int
+        let core_len = (diagram[diagram.len() - 1][diagram[0].len() - 2] as usize) - ('0' as usize);
+        let mut core: Vec<Vec<char>> = vec![Vec::new(); core_len];
+        
+        //Loops through diagram
+        for x in 0..core_len {
+            for y in (0..diagram.len()).rev() {
+                //Formula for x to access correct part of diagram
+                let chr = diagram[y][1 + 4*x];
+                //If there is a crate, add it
+                if chr.is_alphabetic() {
+                    core[x].push(chr);
+                }
+            }
+        }
+
+        Self { core }
+    }
+
+    fn move_crates(&mut self, count: usize, src: usize, dest: usize) {
+        for _ in 0..count {
+            let top_char = self.core[src-1].pop().unwrap();
+            self.core[dest-1].push(top_char);
+        }
+    }
+
+    fn get_message(&self) -> String {
+        let mut res = String::new();
+
+        for stack in &self.core {
+            res.push(*stack.last().unwrap());
+        };
+
+        res
+    }
+}
+
+pub fn v1() {
+    //Read file into line reading buffer
+    let file = File::open("src/input.txt").unwrap();
+    let mut lines = BufReader::new(file)
+        .lines()
+        .map_while(Result::ok);    
+
+    //Gets diagram
+    let mut crate_diagram: Vec<Vec<char>> = vec![];
+    //Up to empty line
+    for line in &mut lines {
+        if line.is_empty() {
+            break;
+        }
+        crate_diagram.push(line.chars().collect());
+    }
+
+    //Creates crates object
+    let mut crates = Crates::from_diagram(crate_diagram);
+
+    //Prints instructions line by line
+    for line in lines {
+        //Split by spaces
+        let words: Vec<&str> = line.split(" ").collect();
+
+        //Parse into ints, passes to crates
+        crates.move_crates(
+            words[1].parse().unwrap(),
+            words[3].parse().unwrap(),
+            words[5].parse().unwrap()
+        )
+    }
+    
+    println!("{}", crates.get_message());
+}
